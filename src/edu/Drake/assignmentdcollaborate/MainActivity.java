524b08382c55eda4bd52c3856e87e5b6ee13d261
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2013-02-18 19:04:45 -0600

    The first screen is done. I've already put in placeholder functions and such for the other two activities and their buttons. Change them if you wish.

--- src/edu/Drake/assignmentdcollaborate/MainActivity.java
+++ src/edu/Drake/assignmentdcollaborate/MainActivity.java
@@ -1,15 +1,50 @@
 package edu.Drake.assignmentdcollaborate;
 
+import edu.Drake.assignmentdcollaborate.R;
+import edu.Drake.assignmentdcollaborate.SecondActivity;
+import edu.Drake.assignmentdcollaborate.ThirdActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "MainActivity";
 
+	Button button;
+	Button buttonToThird;
+	int myVariable = 0;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				myVariable++;
+				Log.v(TAG, "page 1 buttons pressed " + myVariable + " times");
+				Intent intent = new Intent(v.getContext(), SecondActivity.class);
+				startActivity(intent);
+			}
+		});
+		buttonToThird = (Button) findViewById(R.id.buttonToThird);
+		buttonToThird.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				myVariable++;
+				Log.v(TAG, "page 1 buttons pressed " + myVariable + " times");
+				Intent intent = new Intent(v.getContext(), ThirdActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +53,8 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	
+	
 
 }
